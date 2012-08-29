081eddb844fa7274218eeeb87515a7e8b46a7bee
Robert Bor <bor.robert@gmail.com>
2012-08-29 22:35:57 +0200

    Add Authentication details plus first test

---
+++ src/main/java/nl/t42/openstack/model/PasswordCredentials.java
@@ -0,0 +1,21 @@
+package nl.t42.openstack.model;
+
+public class PasswordCredentials {
+
+    private String username;
+
+    private String password;
+
+    public PasswordCredentials(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
