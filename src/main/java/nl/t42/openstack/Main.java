82ae5978bb116061b7735213cd8a29374ab3a0da
Robert Bor <bor.robert@gmail.com>
2012-08-31 16:19:28 +0200

    removed useless code in Main

--- src/main/java/nl/t42/openstack/Main.java
+++ src/main/java/nl/t42/openstack/Main.java
@@ -1,16 +1,6 @@
 package nl.t42.openstack;
 
-import nl.t42.openstack.model.authentication.Authentication;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.SerializationConfig;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String args[]) throws IOException {
@@ -26,30 +16,6 @@ public class Main {
 
         OpenStackClient client = new OpenStackClient();
         client.authenticate(username, password, url);
-
-//        DefaultHttpClient httpClient = new DefaultHttpClient();
-//        HttpPost httpPost = new HttpPost(url);
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(SerializationConfig.Feature.WRAP_ROOT_VALUE, true);
-//        Authentication auth = new Authentication(username, password);
-//        String jsonString = mapper.writeValueAsString(auth);
-//        System.out.println("JSON: "+jsonString);
-//        StringEntity input = new StringEntity(jsonString);
-//        input.setContentType("application/json");
-//        httpPost.setEntity(input);
-//
-//        HttpResponse response = httpClient.execute(httpPost);
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
-//        StringBuilder responseString = new StringBuilder();
-//        System.out.println("Output from Server .... \n");
-//        String output;
-//        while ((output = br.readLine()) != null) {
-//            responseString.append(output);
-//        }
-//        System.out.println(responseString.toString());
-
     }
 
 }
