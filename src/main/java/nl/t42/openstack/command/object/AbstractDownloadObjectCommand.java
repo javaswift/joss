6984465f5e8006e174936d12da25897696a40b2d
Robert Bor <bor.robert@gmail.com>
2012-09-09 18:35:45 +0200

    IOUtils.copy instead of the manual IO plumbing

--- src/main/java/nl/t42/openstack/command/object/AbstractDownloadObjectCommand.java
+++ src/main/java/nl/t42/openstack/command/object/AbstractDownloadObjectCommand.java
@@ -12,7 +12,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpRequestBase;
 
 import java.io.*;
 
--- src/main/java/nl/t42/openstack/command/object/DownloadObjectToFileCommand.java
+++ src/main/java/nl/t42/openstack/command/object/DownloadObjectToFileCommand.java
@@ -4,6 +4,7 @@ import nl.t42.openstack.command.identity.access.Access;
 import nl.t42.openstack.model.Container;
 import nl.t42.openstack.model.StoreObject;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -21,18 +22,12 @@ public class DownloadObjectToFileCommand extends AbstractDownloadObjectCommand<H
 
     @Override
     protected void handleEntity(HttpEntity entity) throws IOException {
-        InputStream input = null;
         OutputStream output = null;
         try {
-            input = entity.getContent();
             output = new FileOutputStream(targetFile);
-            byte[] buffer = new byte[65536];
-            for (int length; (length = input.read(buffer)) > 0;) {
-                output.write(buffer, 0, length);
-            }
+            IOUtils.copy(entity.getContent(), new FileOutputStream(targetFile));
         } finally {
             if (output != null) try { output.close(); } catch (IOException logOrIgnore) {}
-            if (input != null) try { input.close(); } catch (IOException logOrIgnore) {}
         }
     }
 
