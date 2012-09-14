cd3cf4002145c0a19c9cf1eeb433e5d3317473ff
Robert Bor <bor.robert@gmail.com>
2012-09-14 09:01:37 +0200

    Added test for reauthentication

---
+++ src/test/java/nl/tweeenveertig/openstack/command/core/AbstractSecureCommandTest.java
@@ -0,0 +1,37 @@
+package nl.tweeenveertig.openstack.command.core;
+
+import nl.tweeenveertig.openstack.client.impl.AccountImpl;
+import nl.tweeenveertig.openstack.command.container.CreateContainerCommand;
+import nl.tweeenveertig.openstack.command.identity.AuthenticationCommand;
+import nl.tweeenveertig.openstack.command.identity.access.Access;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.io.IOException;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class AbstractSecureCommandTest extends BaseCommandTest {
+
+    @Mock
+    AuthenticationCommand authCommand;
+
+    @Before
+    public void setup() throws IOException {
+        super.setup();
+    }
+
+    @Test
+    public void reauthenticateFailTwice() {
+        when(statusLine.getStatusCode()).thenReturn(401);
+        when(authCommand.call()).thenReturn(new Access());
+        this.account = new AccountImpl(authCommand, httpClient, defaultAccess);
+        try {
+            new CreateContainerCommand(this.account, httpClient, defaultAccess, account.getContainer("containerName")).call();
+        } catch(CommandException err) {
+            assertEquals(CommandExceptionError.UNAUTHORIZED, err.getError());
+        }
+    }
+}
