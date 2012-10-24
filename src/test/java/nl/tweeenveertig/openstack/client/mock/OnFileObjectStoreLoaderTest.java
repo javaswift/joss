6495d91305af2564e398c4d01633c4bf279a683f
Robert Bor <bor.robert@gmail.com>
2012-10-24 09:57:26 +0200

    OnFileObjectStoreLoader test-covered

--- src/test/java/nl/tweeenveertig/openstack/client/mock/OnFileObjectStoreLoaderTest.java
+++ src/test/java/nl/tweeenveertig/openstack/client/mock/OnFileObjectStoreLoaderTest.java
@@ -6,7 +6,9 @@ import nl.tweeenveertig.openstack.client.StoredObject;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -24,6 +26,21 @@ public class OnFileObjectStoreLoaderTest {
         StoredObject object = account.getContainer("container2").getObject("logo.png");
         assertEquals(4670, object.getContentLength());
         assertEquals("image/png", object.getContentType());
+    }
+
+    @Test
+    public void noUrl() throws URISyntaxException {
+        testUrl(null);
+    }
+
+    @Test
+    public void urlOfNoneFileType() throws URISyntaxException, MalformedURLException {
+        testUrl(new URL("http://java.sun.com/index.html"));
+    }
+
+    protected void testUrl(URL url) throws URISyntaxException {
+        OnFileObjectStoreLoader loader = new OnFileObjectStoreLoader();
+        assertEquals(0, loader.listFiles(url).length);
     }
 
 }
