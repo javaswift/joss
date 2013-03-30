746484fedd0d693a2298f4e61910ac9c1df9d9f2
Robert Bor <bor.robert@gmail.com>
2013-03-30 13:28:21 +0100

    Issue #5 - Caught UnsupportedEncodingException in QueryParameter

--- src/test/java/org/javaswift/joss/instructions/QueryParameterTest.java
+++ src/test/java/org/javaswift/joss/instructions/QueryParameterTest.java
@@ -1,9 +1,20 @@
 package org.javaswift.joss.instructions;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( { QueryParameter.class, URLEncoder.class } )
 public class QueryParameterTest {
 
     @Test
@@ -22,6 +33,14 @@ public class QueryParameterTest {
     public void getQueryWithValueNull() {
         QueryParameter qp = new QueryParameter("marker", null);
         assertEquals(null, qp.getQuery());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void encodingThrowsException() throws UnsupportedEncodingException {
+        mockStatic(URLEncoder.class);
+        when(URLEncoder.encode(any(String.class), any(String.class))).thenThrow(new UnsupportedEncodingException());
+        QueryParameter qp = new QueryParameter("abc", "def");
+        qp.getQuery();
     }
 
 }
