c0ac8bfe905828974d4838c7cefc19d9c83805f2
Robert Bor <bor.robert@gmail.com>
2013-05-12 11:52:10 +0200

    Issue #30 - only when exactly one enabled tenant is available, will one be chosen. Otherwise, an exception is thrown

--- src/main/java/org/javaswift/joss/command/shared/identity/tenant/Tenants.java
+++ src/main/java/org/javaswift/joss/command/shared/identity/tenant/Tenants.java
@@ -2,6 +2,7 @@ package org.javaswift.joss.command.shared.identity.tenant;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.javaswift.joss.exception.CommandException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +14,44 @@ public class Tenants {
 
     @JsonIgnore
     public Tenant getTenant() {
-        return this.tenants.get(0);
+        List<Tenant> enabledTenants = getEnabledTenants();
+        if (enabledTenants.size() < 1) {
+            throw new CommandException("No enabled tenant found during auto-discovery");
+        }
+        if (enabledTenants.size() > 1) {
+            throw new CommandException("Multiple enabled tenants found. Please configure one to use: "+getTenantString(enabledTenants));
+        }
+        return enabledTenants.get(0);
+    }
+
+    @JsonIgnore
+    public String getTenantString(List<Tenant> tenants) {
+        StringBuilder returnString = new StringBuilder();
+        boolean first = true;
+        for (Tenant tenant : tenants) {
+            if (!first) {
+                returnString.append(", ");
+            }
+            returnString
+                    .append("[ID:")
+                    .append(tenant.id)
+                    .append("/Name:")
+                    .append(tenant.name)
+                    .append("]");
+            first = false;
+        }
+        return returnString.toString();
+    }
+
+    @JsonIgnore
+    public List<Tenant> getEnabledTenants() {
+        List<Tenant> enabledTenants = new ArrayList<Tenant>();
+        for (Tenant tenant : tenants) {
+            if (tenant.enabled) {
+                enabledTenants.add(tenant);
+            }
+        }
+        return enabledTenants;
     }
 
 }
--- src/test/java/org/javaswift/joss/command/shared/identity/tenant/TenantsTest.java
+++ src/test/java/org/javaswift/joss/command/shared/identity/tenant/TenantsTest.java
@@ -1,16 +1,19 @@
 package org.javaswift.joss.command.shared.identity.tenant;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.javaswift.joss.exception.CommandException;
 import org.junit.Test;
 
 import java.io.IOException;
 
+import static junit.framework.Assert.fail;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TenantsTest {
 
     @Test
-    public void testMarshalling() throws IOException {
+    public void marshalling() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         Tenant tenant = new Tenant();
         tenant.enabled = true;
@@ -19,6 +22,44 @@ public class TenantsTest {
         Tenants tenants = new Tenants();
         tenants.tenants.add(tenant);
         assertEquals("{\"tenants\":[{\"id\":\"tenant-id\",\"name\":\"tenant-name\",\"enabled\":true}]}", mapper.writeValueAsString(tenants));
+    }
+
+    @Test(expected = CommandException.class)
+    public void noTenants() {
+        Tenants tenants = new Tenants();
+        tenants.getTenant();
+    }
+
+    @Test
+    public void tooManyTenants() {
+        Tenants tenants = new Tenants();
+        tenants.tenants.add(createTenant("#1", "Alice", true));
+        tenants.tenants.add(createTenant("#2", "Bob", true));
+        try {
+            tenants.getTenant();
+            fail("Should have thrown an exception");
+        } catch (CommandException err) {
+            assertTrue(err.getMessage().contains("[ID:#1/Name:Alice]"));
+            assertTrue(err.getMessage().contains("[ID:#2/Name:Bob]"));
+        }
+    }
+
+    @Test
+    public void onlyOneEnabledTenant() {
+        Tenants tenants = new Tenants();
+        tenants.tenants.add(createTenant("#1", "Alice", false));
+        tenants.tenants.add(createTenant("#2", "Bob", true));
+        tenants.tenants.add(createTenant("#3", "Carol", false));
+        Tenant tenant = tenants.getTenant();
+        assertEquals("Bob", tenant.name);
+    }
+
+    protected Tenant createTenant(String id, String name, boolean enabled) {
+        Tenant tenant = new Tenant();
+        tenant.id = id;
+        tenant.name = name;
+        tenant.enabled = enabled;
+        return tenant;
     }
 
 }
