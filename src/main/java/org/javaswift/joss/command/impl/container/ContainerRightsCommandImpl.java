e2bb29fd1a30f5e20d5628914aa49dea96a8787b
zhangshaowen <zhang_shaowen@139.com>
2016-07-28 09:01:48 +0800

    #107:Fix a bug in setting acl for existed container. Now the method  makePublic will send a post request other than a put request to the swift  server.

--- src/main/java/org/javaswift/joss/command/impl/container/ContainerRightsCommandImpl.java
+++ src/main/java/org/javaswift/joss/command/impl/container/ContainerRightsCommandImpl.java
@@ -2,7 +2,7 @@ package org.javaswift.joss.command.impl.container;
 
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpPost;
 import org.javaswift.joss.command.impl.core.httpstatus.HttpStatusChecker;
 import org.javaswift.joss.command.impl.core.httpstatus.HttpStatusFailCondition;
 import org.javaswift.joss.command.impl.core.httpstatus.HttpStatusMatch;
@@ -15,7 +15,7 @@ import org.javaswift.joss.model.Access;
 import org.javaswift.joss.model.Account;
 import org.javaswift.joss.model.Container;
 
-public class ContainerRightsCommandImpl extends AbstractContainerCommand<HttpPut, String[]> implements ContainerRightsCommand {
+public class ContainerRightsCommandImpl extends AbstractContainerCommand<HttpPost, String[]> implements ContainerRightsCommand {
 
     public ContainerRightsCommandImpl(Account account, HttpClient httpClient, Access access, Container container, boolean publicContainer) {
         super(account, httpClient, access, container);
@@ -29,14 +29,15 @@ public class ContainerRightsCommandImpl extends AbstractContainerCommand<HttpPut
     }
 
     @Override
-    protected HttpPut createRequest(String url) {
-        return new HttpPut(url);
+    protected HttpPost createRequest(String url) {
+        return new HttpPost(url);
     }
 
     @Override
     public HttpStatusChecker[] getStatusCheckers() {
         return new HttpStatusChecker[] {
             new HttpStatusSuccessCondition(new HttpStatusMatch(HttpStatus.SC_ACCEPTED)),
+            new HttpStatusSuccessCondition(new HttpStatusMatch(HttpStatus.SC_NO_CONTENT)),
             new HttpStatusFailCondition(new HttpStatusMatch(HttpStatus.SC_NOT_FOUND))
         };
     }
