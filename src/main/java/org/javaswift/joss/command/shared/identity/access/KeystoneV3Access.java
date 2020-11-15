8cd4b9a6feff5713a76b43073294c6fc515a71ed
Balu S <balu.s@6dtech.co.in>
2020-11-15 23:02:14 +0530

    Fix for non scoped auth with no catalog in response

--- src/main/java/org/javaswift/joss/command/shared/identity/access/KeystoneV3Access.java
+++ src/main/java/org/javaswift/joss/command/shared/identity/access/KeystoneV3Access.java
@@ -10,108 +10,111 @@ import org.javaswift.joss.model.Access;
 
 public class KeystoneV3Access implements Access {
 
-    private final String token;
-    private final Collection<EndPoint> endPoints;
+	private final String token;
+	private final Collection<EndPoint> endPoints;
 
-    private EndPoint currentEndpoint;
+	private EndPoint currentEndpoint;
 
-    public KeystoneV3Access(String token, JsonNode response) {
-        this.token = token;
-        this.endPoints = getEndPoints(response);
-        this.currentEndpoint = getEndPoint(null);
-    }
+	public KeystoneV3Access(String token, JsonNode response) {
+		this.token = token;
+		this.endPoints = getEndPoints(response);
+		this.currentEndpoint = getEndPoint(null);
+	}
 
-    @Override
-    public void setPreferredRegion(String preferredRegion) {
-        this.currentEndpoint = getEndPoint(preferredRegion);
-    }
+	@Override
+	public void setPreferredRegion(String preferredRegion) {
+		this.currentEndpoint = getEndPoint(preferredRegion);
+	}
 
-    @Override
-    public String getToken() {
-        return token;
-    }
+	@Override
+	public String getToken() {
+		return token;
+	}
 
-    @Override
-    public String getInternalURL() {
-        return currentEndpoint.internalURL;
-    }
+	@Override
+	public String getInternalURL() {
+		return currentEndpoint.internalURL;
+	}
 
-    @Override
-    public String getPublicURL() {
-        return currentEndpoint.publicURL;
-    }
+	@Override
+	public String getPublicURL() {
+		return currentEndpoint.publicURL;
+	}
 
-    /**
-     * @return Always true, v3 doesn't use the tenant ID for auth
-     */
-    @Override
-    public boolean isTenantSupplied() {
-        return true;
-    }
+	/**
+	 * @return Always true, v3 doesn't use the tenant ID for auth
+	 */
+	@Override
+	public boolean isTenantSupplied() {
+		return true;
+	}
 
-    @Override
-    public String getTempUrlPrefix(TempUrlHashPrefixSource tempUrlHashPrefixSource) {
-        final String tempUrlPrefix;
-        if (tempUrlHashPrefixSource == null) {
-            tempUrlPrefix = "";
-        } else if (tempUrlHashPrefixSource == TempUrlHashPrefixSource.PUBLIC_URL_PATH) {
-            tempUrlPrefix = TempUrlHashPrefixSource.getPath(currentEndpoint.publicURL);
-        } else if (tempUrlHashPrefixSource == TempUrlHashPrefixSource.INTERNAL_URL_PATH) {
-            tempUrlPrefix = TempUrlHashPrefixSource.getPath(currentEndpoint.internalURL);
-        } else {
-            tempUrlPrefix = TempUrlHashPrefixSource.getPath(currentEndpoint.adminURL);
-        }
-        return tempUrlPrefix.endsWith("/") ? tempUrlPrefix.substring(0, tempUrlPrefix.length()-1) : tempUrlPrefix;
-    }
+	@Override
+	public String getTempUrlPrefix(TempUrlHashPrefixSource tempUrlHashPrefixSource) {
+		final String tempUrlPrefix;
+		if (tempUrlHashPrefixSource == null) {
+			tempUrlPrefix = "";
+		} else if (tempUrlHashPrefixSource == TempUrlHashPrefixSource.PUBLIC_URL_PATH) {
+			tempUrlPrefix = TempUrlHashPrefixSource.getPath(currentEndpoint.publicURL);
+		} else if (tempUrlHashPrefixSource == TempUrlHashPrefixSource.INTERNAL_URL_PATH) {
+			tempUrlPrefix = TempUrlHashPrefixSource.getPath(currentEndpoint.internalURL);
+		} else {
+			tempUrlPrefix = TempUrlHashPrefixSource.getPath(currentEndpoint.adminURL);
+		}
+		return tempUrlPrefix.endsWith("/") ? tempUrlPrefix.substring(0, tempUrlPrefix.length() - 1) : tempUrlPrefix;
+	}
 
-    private EndPoint getEndPoint(String region) {
-        if (endPoints.isEmpty()) {
-            throw new IllegalStateException("No endpoints available");
-        }
+	private EndPoint getEndPoint(String region) {
+		if (endPoints.isEmpty()) {
+			throw new IllegalStateException("No endpoints available");
+		}
 
-        if (region == null) {
-            return endPoints.iterator().next();
-        }
+		if (region == null) {
+			return endPoints.iterator().next();
+		}
 
-        for (EndPoint endPoint : endPoints) {
-            if (region.equals(endPoint.region)) {
-                return endPoint;
-            }
-        }
+		for (EndPoint endPoint : endPoints) {
+			if (region.equals(endPoint.region)) {
+				return endPoint;
+			}
+		}
 
-        throw new IllegalArgumentException("No endpoint for region: " + region);
-    }
+		throw new IllegalArgumentException("No endpoint for region: " + region);
+	}
 
-    private Collection<EndPoint> getEndPoints(JsonNode response) {
-        Map<String, EndPoint> result = new LinkedHashMap<String, EndPoint>();
+	private Collection<EndPoint> getEndPoints(JsonNode response) {
+		Map<String, EndPoint> result = new LinkedHashMap<String, EndPoint>();
+		JsonNode token = response.get("token");
+		if (token.get("catalog") != null) {
+			for (JsonNode catalog : token.get("catalog")) {
+				if ("object-store".equals(catalog.get("type").asText())) {
+					for (JsonNode endpointNode : catalog.get("endpoints")) {
+						String interfaceType = endpointNode.get("interface").asText();
+						String url = endpointNode.get("url").asText();
+						String region = endpointNode.get("region").asText();
+						;
 
-        JsonNode token = response.get("token");
-        for (JsonNode catalog : token.get("catalog")) {
-            if ("object-store".equals(catalog.get("type").asText())) {
-                for (JsonNode endpointNode : catalog.get("endpoints")) {
-                    String interfaceType = endpointNode.get("interface").asText();
-                    String url = endpointNode.get("url").asText();
-                    String region = endpointNode.get("region").asText();;
+						EndPoint endPoint = result.get(region);
 
-                    EndPoint endPoint = result.get(region);
+						if (endPoint == null) {
+							endPoint = new EndPoint();
+							endPoint.region = region;
+							result.put(region, endPoint);
+						}
 
-                    if (endPoint == null) {
-                        endPoint = new EndPoint();
-                        endPoint.region = region;
-                        result.put(region, endPoint);
-                    }
+						if ("public".equals(interfaceType)) {
+							endPoint.publicURL = url;
+						} else if ("admin".equals(interfaceType)) {
+							endPoint.adminURL = url;
+						} else if ("internal".equals(interfaceType)) {
+							endPoint.internalURL = url;
+						}
+					}
+				}
+			}
 
-                    if ("public".equals(interfaceType)) {
-                        endPoint.publicURL = url;
-                    } else if ("admin".equals(interfaceType)) {
-                        endPoint.adminURL = url;
-                    } else if ("internal".equals(interfaceType)) {
-                        endPoint.internalURL = url;
-                    }
-                }
-            }
-        }
+		}
 
-        return result.values();
-    }
+		return result.values();
+	}
 }
